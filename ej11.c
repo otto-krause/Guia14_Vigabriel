815c84495697ab83566cf171564c08054d8d2812
Gastonviga <87142425+Gastonviga@users.noreply.github.com>
2021-11-15 21:53:46 -0300

    Create ej11.c

---
+++ ej11.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void CadenaInvertida(char *C, char *CI, int L, int I, int N);
+
+int main()
+{
+    char Cadena[20], CadenaI[20];
+    int L=0, I=0, N=0;
+
+    printf("Ingrese una cadena de texto: ");
+    gets(Cadena);
+
+    L=strlen(Cadena);
+
+    system("cls");
+
+    CadenaInvertida(Cadena, CadenaI, L, I, N);
+
+    return 0;
+}
+
+void CadenaInvertida(char *C, char *CI, int L, int I, int N)
+{
+    for (I=L-1; I>=0; I--)
+    {
+        CI[N]=C[I];
+        N++;
+    }
+
+    puts(CI);
+}
